4f1daf4a10f79db6f2ecc873eb364c589f14e68e
ChristopherPossonato <christophrpossonato@gmail.com>
2024-04-19 14:31:12 -0300

    ajustes pacotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  resolve: {
+    fallback: {
+        "path": require.resolve("path-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "util": require.resolve("util/"),
+        "buffer": require.resolve("buffer/"),
+        "querystring": require.resolve("querystring-es3"),
+        "fs": false ,
+        "http": require.resolve("stream-http"),
+        "net": false,
+        "zlib": require.resolve("browserify-zlib"),
+        "crypto": require.resolve("crypto-browserify"),
+        "assert": require.resolve("assert/"),
+        "vm": require.resolve("vm-browserify"),
+        "async_hooks": require.resolve("async_hooks"),
+        "async_hooks": require.resolve("raw-body")
+    }
+}
 };
